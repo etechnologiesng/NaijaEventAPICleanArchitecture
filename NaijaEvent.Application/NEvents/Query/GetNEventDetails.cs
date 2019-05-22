9f85b0ffc8a5a568ebe9883180a0e77d79fe86b7
Emmanuel Enyinnaya <etechnologiesng@outlook.com>
2019-05-22 14:33:51 +0100

    Implemented AutoMapper

---
+++ NaijaEvent.Application/NEvents/Query/GetNEventDetails.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using NaijaEvent.Application.Common;
+using NaijaEvent.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaijaEvent.Application.NEvent.Query
+{
+   public class GetNEventDetails :  IRequest<NEventDTO>
+    {
+
+        public Guid nEventId { get; set; }
+        public string nEventName { get; set; }
+    }
+}
---
+++ NaijaEvent.Application/NEvents/Query/GetNEventDetailsHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using NaijaEvent.Application.DTOs;
+using NaijaEvent.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NaijaEvent.Application.NEvent.Query
+{
+    class GetNEventDetailsHandler : IRequestHandler<GetNEventDetails, NEventDTO>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetNEventDetailsHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<NEventDTO> Handle(GetNEventDetails request, CancellationToken cancellationToken)
+        {
+            var result = await _unitOfWork.NEvent.FindAsync(b => b.nEventid == request.nEventId);
+
+            return (result) ? _mapper.ProjectTo<NEventDTO>(result) : null;
+        }
+    }
+}
