a0dc368be13d920c81d5051f01744041eb244c69
Emmanuel Enyinnaya <etechnologiesng@outlook.com>
2019-05-23 22:46:32 +0100

    Added fluent validation

---
+++ NaijaEvent.Application/NEvents/Command/CreateEvent/CreateEventCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaijaEvent.Application.NEvents.Command.CreateEvent
+{
+   public class CreateEventCommandValidator : AbstractValidator<CreateEventCommand>
+    {
+
+        public CreateEventCommandValidator()
+        {
+            RuleFor(x => x.EventId).NotNull();
+            RuleFor(x => x.EventName).MaximumLength(60).NotEmpty();
+            RuleFor(x => x.Description).MaximumLength(60).NotEmpty();
+            RuleFor(x => x.StartDate).NotNull();
+            RuleFor(x => x.EndDate).NotNull();
+
+        }
+
+       
+    }
+}
