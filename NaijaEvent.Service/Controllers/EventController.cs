99a6e7d59132b1d1df7177130178039fc07f2caf
Emmanuel Enyinnaya <etechnologiesng@outlook.com>
2019-05-29 15:16:52 +0100

    Presentation Layer

--- NaijaEvent.Service/Controllers/EventController.cs
+++ NaijaEvent.Service/Controllers/EventController.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
 using System.Threading.Tasks;
 using MediatR;
-using Microsoft.AspNetCore.Http;
+
 using Microsoft.AspNetCore.Mvc;
 using NaijaEvent.Application.DTOs;
 using NaijaEvent.Application.NEvents.Command.CreateEvent;
